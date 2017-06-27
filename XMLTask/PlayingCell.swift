903b11094204a3a423bfd58fe5714512b5c207fb
Blake Lockley <bwlockley@gmail.com>
2017-06-27 21:47:49 +1000

    UI: remove playingCell progress bar

--- XMLTask/PlayingCell.swift
+++ XMLTask/PlayingCell.swift
@@ -10,23 +10,11 @@ import UIKit
 
 class PlayingCell: PlayoutItemCell {
 
-  @IBOutlet weak var fullBar: UIView!
-  @IBOutlet weak var progressBar: UIView!
-  @IBOutlet weak var progressTrailing: NSLayoutConstraint!
   @IBOutlet weak var durationLabel: UILabel!
 
   override func initWith(playoutItem: PlayoutItem) {
     super.initWith(playoutItem: playoutItem)
     durationLabel.text = playoutItem.prettyDuration()
-    updateProgress()
-  }
-
-  func updateProgress() {
-    let progress = CGFloat(playoutItem.progress())
-    let full = fullBar.frame.width - 2.0 //for borders
-
-    progressTrailing.constant = full * (1 - progress) + 1
-
   }
 
 }
